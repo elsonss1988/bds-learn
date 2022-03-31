6c571b44b4b3a36e4e16da4c04f834f01bc3e16c
Elson Santos <45674699+elsonss1988@users.noreply.github.com>
2022-03-31 08:33:17 -0300

    User, Role

---
+++ src/main/java/com/devsuperior/dslearnbds/entities/Role.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dslearnbds.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name="tb_role")
+public class Role implements Serializable {
+    public static final Long SerialVersionUID=1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String authority;
+
+    public Role(){};
+
+    public Role(long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
